6c1e7fc78a00e2056cd4745e228d7fa26f86d333
Tuan Nguyen <tuannguyen221101@gmail.com>
2023-11-29 22:00:28 +0200

    initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,90 @@
-import React from 'react'
+import { useState } from 'react'
+import { ToastContainer } from 'react-toastify'
+import { Box, Container, CssBaseline, Typography } from '@mui/material'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+
+import 'react-toastify/dist/ReactToastify.css'
+
+import { ThemeContext } from './contexts/ThemeContext'
+import ThemeDisplay from './components/ThemeDisplay'
+import Footer from './components/Footer'
+import ErrorPage from './pages/ErrorPage'
+import Navbar from './components/Navbar'
 
 const App = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState<boolean>(true)
+
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <ThemeContext.Provider value={{ isDarkTheme, setIsDarkTheme }}>
+        <ThemeDisplay>
+          <CssBaseline />
+          <Navbar />
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={true}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+          />
+          <Box
+            sx={{
+              bgcolor: 'background.paper',
+              pt: 4,
+            }}
+          >
+            <Container maxWidth="md">
+              <Link to="/" style={{ textDecoration: 'none' }}>
+                <Typography
+                  component="h1"
+                  variant="h2"
+                  align="center"
+                  color="text.primary"
+                  gutterBottom
+                >
+                  OpenLibSys
+                </Typography>
+              </Link>
+              <Typography
+                variant="h5"
+                align="center"
+                color="text.secondary"
+                paragraph
+              >
+                A portal to look for books of any categories and/or authors of
+                your choice, from Charles Dickens to Mark Twain.
+              </Typography>
+            </Container>
+          </Box>
+
+          <main style={{ minHeight: 'calc(100vh - 290px)' }}>
+            <Container sx={{ py: 8 }} maxWidth="lg">
+              <Routes>
+                <Route path="/" element={null} />
+                <Route path="/books" element={null} />
+                <Route path="/books">
+                  <Route path=":id" element={null} />
+                </Route>
+                <Route path="/profile" element={null} />
+                <Route path="/login" element={null} />
+                <Route path="/register" element={null} />
+                <Route path="/cart" element={null} />
+                <Route path="*" element={<ErrorPage />} />
+              </Routes>
+            </Container>
+          </main>
+
+          <footer style={{ backgroundColor: 'gray' }}>
+            <Footer />
+          </footer>
+        </ThemeDisplay>
+      </ThemeContext.Provider>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,99 @@
+import React from 'react'
+import { AppBar, Stack, Toolbar, Typography } from '@mui/material'
+import { Link } from 'react-router-dom'
+
+import { useThemeContext } from '../hooks/useThemeContext'
+import MaterialUISwitch from './MaterialUISwitch'
+
+const Navbar = () => {
+  const { isDarkTheme, setIsDarkTheme } = useThemeContext()
+
+  const handleSwitchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (setIsDarkTheme) {
+      setIsDarkTheme(event.target.checked)
+    }
+  }
+
+  return (
+    <AppBar position="sticky" color="primary" enableColorOnDark>
+      <Toolbar
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Stack direction="row" spacing={5}>
+          <Link to="/" style={{ textDecoration: 'none' }}>
+            <Typography variant="h6" color="inherit" noWrap>
+              Home
+            </Typography>
+          </Link>
+          <Link to="/books" style={{ textDecoration: 'none' }}>
+            <Typography variant="h6" color="inherit" noWrap>
+              All books
+            </Typography>
+          </Link>
+        </Stack>
+        <Stack direction="row" spacing={5}>
+          <MaterialUISwitch
+            checked={isDarkTheme}
+            onChange={handleSwitchChange}
+          />
+          <Link to="/cart" style={{ textDecoration: 'none' }}>
+            {/* <Badge badgeContent={totalCartItems} color="warning">
+              <Typography variant="h6" color="inherit" noWrap>
+                🛒
+              </Typography>
+            </Badge> */}
+            <Typography variant="h6" color="inherit" noWrap>
+              🛒
+            </Typography>
+          </Link>
+          <Stack direction="row" spacing={5}>
+            <Link to="/register" style={{ textDecoration: 'none' }}>
+              <Typography variant="h6" color="inherit" noWrap>
+                Sign up
+              </Typography>
+            </Link>
+            <Link to="/login" style={{ textDecoration: 'none' }}>
+              <Typography variant="h6" color="inherit" noWrap>
+                Login
+              </Typography>
+            </Link>
+          </Stack>
+          {/* {isLoggedIn ? (
+            <Stack direction="row" spacing={5}>
+              <Link to="/profile" style={{ textDecoration: 'none' }}>
+                <Avatar src={user?.avatar} />
+              </Link>
+              <Link
+                to="/"
+                style={{ textDecoration: 'none' }}
+                onClick={() => dispatch(userLogout())}
+              >
+                <Typography variant="h6" color="inherit" noWrap>
+                  Log out
+                </Typography>
+              </Link>
+            </Stack>
+          ) : (
+            <Stack direction="row" spacing={5}>
+              <Link to="/register" style={{ textDecoration: 'none' }}>
+                <Typography variant="h6" color="inherit" noWrap>
+                  Sign up
+                </Typography>
+              </Link>
+              <Link to="/login" style={{ textDecoration: 'none' }}>
+                <Typography variant="h6" color="inherit" noWrap>
+                  Login
+                </Typography>
+              </Link>
+            </Stack>
+          )} */}
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Navbar
